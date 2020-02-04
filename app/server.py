d34620c5e790be4b602fcd6681ab0fc0dcc7c195
daquarti <48704929+daquarti@users.noreply.github.com>
2020-02-04 17:08:45 -0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1-NZDNbD76Q2T0nBBhS0MWKbOoNjoMQ'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-NZDNbD76Q2T0nBBhS0MWKbOoNjoMQdk'
 export_file_name = 'export.pkl'
 
 classes = ['TTR', 'Negativas']
